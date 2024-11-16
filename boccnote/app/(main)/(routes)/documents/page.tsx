692f9f09140d8ff79e95f7b65510037bc6862163
Huy Pham <huyhoangpham100200@gmail.com>
2024-11-16 19:13:18 +0700

    finish boccnote project

--- boccnote/app/(main)/(routes)/documents/page.tsx
+++
@@ -1,9 +0,0 @@
-const DocumentsPage = () => {
-    return (
-        <div className=" ">
-            This is a protected Documents page
-        </div>
-    );
-}
-
-export default DocumentsPage;
---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      "files.edgestore.dev"
+    ]
+  }
+  /* config options here */
+};
+
+export default nextConfig;
